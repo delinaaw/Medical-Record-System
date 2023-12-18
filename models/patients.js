d47d9158dbc5d7ff8d341bec5c1ef71877fe1a00
Ofa Partick <ofapatrick04@gmail.com>
2023-12-18 10:12:10 +0100

    Implement Patient model

---
+++ models/patients.js
@@ -0,0 +1,19 @@
+// models/Patient.js
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+    surname: { type: String, required: true },
+    otherNames: { type: String, required: true },
+    gender: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    residentialAddress: { type: String, required: true },
+    emergencyContact: {
+        name: { type: String, required: true },
+        phone: { type: String, required: true },
+        relationship: { type: String, required: true },
+    },
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+module.exports = Patient;
